af793b7e9bd15bad627df9e617650ef0ab2f28db
Redsart <redsartvimes@yahoo.com>
2019-04-23 14:36:24 +0300

    Change names in the project.

---
+++ TodoApp.ConsoleApp/ConsoleAppMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TodoApp.Library;
+
+namespace TodoApp.ConsoleApp
+{
+    class ConsoleAppMain
+    {
+        static void Main(string[] args)
+        {
+            string title = "Do this thing";
+            string message = "do something to finish the task";
+
+            var task = new Task(title, message, 5);
+            Console.WriteLine(task.StartDate);
+            Console.WriteLine(task.EndDate);
+            Console.WriteLine(task);
+        }
+    }
+}
---
+++ TodoApp.Library/XMLTaskReader.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoApp.Library
+{
+    class XMLTaskReader
+    {
+    }
+}
