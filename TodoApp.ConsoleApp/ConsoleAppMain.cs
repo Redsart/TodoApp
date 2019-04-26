6ef072feb52caf9b9fc108929aefd6c602af76bd
Redsart <redsartvimes@yahoo.com>
2019-04-26 15:49:11 +0300

    Add TaskReader.cs to read the tasks from xml file!

--- TodoApp.ConsoleApp/ConsoleAppMain.cs
+++ TodoApp.ConsoleApp/ConsoleAppMain.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using TodoApp.Library.Models;
 using TodoApp.Library.Data;
+using System.Globalization;
+using System.Xml;
+using System.Collections;
+
 
 
 namespace TodoApp.ConsoleApp
@@ -8,27 +12,40 @@ namespace TodoApp.ConsoleApp
     class ConsoleAppMain
     {
         static void Main(string[] args)
+
         {
-            Console.Write("Enter a title: ");
-            string title = Console.ReadLine();
-            Console.Write("Enter a description: ");
-            string message = Console.ReadLine();
-            int deadLine = int.Parse(Console.ReadLine());
+            //Console.Write("Enter a title: ");
+            //string title = Console.ReadLine();
+            //Console.Write("Enter a description: ");
+            //string message = Console.ReadLine();
+            //int deadLine = int.Parse(Console.ReadLine());
 
-            var task = new Task(title, message, deadLine);
-            Console.WriteLine("Do you want to save this task? Yes/No");
+            //var task = new Task(title, message, deadLine);
+            //Console.WriteLine("Do you want to save this task? Yes/No");
 
-            string choice = Console.ReadLine();
+            //string choice = Console.ReadLine();
 
-            if (choice == "Yes")
+            //if (choice == "Yes")
+            //{
+            //    XMLTaskWriter writer = new XMLTaskWriter();
+            //    writer.Save(task);
+            //    Console.WriteLine("Save completed!");
+            //}
+            //if (choice == "No")
+            //{
+            //    Console.WriteLine("Good luck!");
+            //}
+
+
+            XMLTaskReader reader = new XMLTaskReader();
+
+            System.Collections.Generic.List<Task> tasks = reader.ReadTasks("../../tasks.xml");
+
+            int count = 1;
+            foreach (var item in tasks)
             {
-                XMLTaskWriter writer = new XMLTaskWriter();
-                writer.Save(task);
-                Console.WriteLine("Save completed!");
-            }
-            if (choice == "No")
-            {
-                Console.WriteLine("Good luck!");
+                Console.WriteLine("Task {0}:/n{1}/nDescription: {2}/nstarted at: {3}/nterm to: {4}",
+                    count,item.Title,item.Message,item.StartDate,item.EndDate);
             }
         }
     }
--- TodoApp.Library/Data/XMLTaskReader.cs
+++ TodoApp.Library/Data/XMLTaskReader.cs
@@ -5,11 +5,42 @@ using TodoApp.Library.Models;
 using System.Xml;
 using System.Xml.Linq;
 using System.IO;
+using System.Globalization;
 
 namespace TodoApp.Library.Data
 {
-    class XMLTaskReader
+    public class XMLTaskReader
     {
+        public TimeSpan TimeLeft(DateTime startDate,DateTime endDate)
+        {
+            TimeSpan timeLeft = endDate.Subtract(startDate).Duration();
 
+            return timeLeft;
+        }
+
+        public List<Task> ReadTasks(string path)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+            XmlElement root = doc.DocumentElement;
+            XmlNodeList nodes;
+            nodes = doc.SelectNodes("tasks/task");
+            List<Task> tasks = new List<Task>();
+
+            foreach (XmlNode node in nodes)
+            {
+                string title = node.SelectSingleNode("title").InnerText;
+                string message = node.SelectSingleNode("description").InnerText;
+                string start = node.SelectSingleNode("dateCreated").InnerText;
+                DateTime startDate = DateTime.ParseExact(start, "dd MM yyyy", CultureInfo.InvariantCulture);
+                string end = node.SelectSingleNode("deadline").InnerText;
+                DateTime endDate = DateTime.ParseExact(end, "dd MM yyyy", CultureInfo.InvariantCulture);
+                int deadline = TimeLeft(startDate, endDate).Days;
+                Task task = new Task(title,message,deadline);
+                tasks.Add(task);
+            }
+
+            return tasks;
+        }
     }
 }
