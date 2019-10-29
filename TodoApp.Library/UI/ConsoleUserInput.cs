628bac41eabf42996935955657659bcc2df7858a
Vencislav Evlogiev <redsartvimes@yahoo.com>
2019-10-29 22:20:21 +0200

    Add Validate class, move user input into a separate class.

---
+++ TodoApp.Library/UI/ConsoleUserInput.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace TodoApp.Library.UI
+{
+    public static class ConsoleUserInput
+    {
+        public static string ReadText(bool required = false)
+        {
+            string text = Console.ReadLine();
+
+            if (required == true)
+            {
+                while (string.IsNullOrEmpty(text))
+                {
+                    Console.WriteLine("This field can't be empty!");
+                    text = Console.ReadLine();
+                }
+            }
+            else
+            {
+                text = Console.ReadLine();
+            }
+
+            return text;
+        }
+
+        public static int ReadOption(string question, string[] availableOptions)
+        {
+            Console.WriteLine(question);
+            Console.WriteLine("Press the number of youre choice!");
+            for (int i = 0; i < availableOptions.Length; i++)
+            {
+                Console.WriteLine("{0}: {1}", i + 1, availableOptions[i]);
+            }
+
+            int number=0;
+            string input = ReadText(true);
+            while (!Validate.IsCorectNumber(input, availableOptions.Length))
+            {
+                input = ReadText(true);
+            }
+            number = int.Parse(input);
+
+            return number;
+        }
+
+        public static bool ReadYesNo(string question, bool defaultValue = false)
+        {
+            Console.WriteLine(question + "yes/no");
+            string choice = ReadText(true);
+
+            while (!Validate.IsYesOrNo(choice))
+            {
+                Console.WriteLine(question + "yes/no");
+                choice = ReadText(true);
+            }
+
+            if (string.Compare(choice, "yes", StringComparison.OrdinalIgnoreCase) == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
+
---
+++ TodoApp.Library/UI/Validate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TodoApp.Library.UI
+{
+    static class Validate
+    {
+        public static bool IsCorectNumber(string number, int length)
+        {
+            bool isTrue = false;
+            int n = 0;
+            if (!int.TryParse(number,out n))
+            {
+                Console.WriteLine("{0} is not a number!", number);
+            }
+
+            else if (n < 1 || n > length)
+            {
+                Console.WriteLine("The number must be in range 1 - {0}", length);
+            }
+
+            else
+            {
+                isTrue = true;
+            }
+
+            return isTrue;
+        }
+
+        public static bool IsYesOrNo(string text)
+        {
+            bool isTrue;
+
+            if (string.Compare(text, "yes", StringComparison.OrdinalIgnoreCase) == 0 || string.Compare(text, "no", StringComparison.OrdinalIgnoreCase) == 0)
+            {
+                return isTrue = true;
+            }
+            else
+            {
+                isTrue = false;
+            }
+
+            return isTrue;
+        }
+    }
+}
