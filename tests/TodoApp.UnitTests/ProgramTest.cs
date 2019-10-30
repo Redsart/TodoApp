3432bf4cd691402857b217c424843e9460c017e8
Drake Kaempfer <dkaempfer46@gmail.com>
2019-10-29 21:26:43 -0500

    tests for save and delete

--- tests/TodoApp.UnitTests/ProgramTest.cs
+++ tests/TodoApp.UnitTests/ProgramTest.cs
@@ -1,11 +1,14 @@
 using System;
 using System.IO;
+using System.Text;
 using Xunit;
 
 namespace TodoApp.UnitTests
 {
     public class ProgramTest
     {
+        private const string testTaskName = "TestTask";
+
         [Fact]
         public void Test_initial_datafile()
         {
@@ -21,6 +24,124 @@ namespace TodoApp.UnitTests
 
                     Assert.Contains("Task 1:\nLearn XML", result);
                     Assert.Contains("Task 2:\nCoffe Time", result);
+                }
+            }
+        }
+
+        [Fact]
+        public void Test_add_task_nosave()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("1"); // manipulate
+            sb.AppendLine("1"); // make
+            sb.AppendLine(testTaskName); // title
+            sb.AppendLine("MyDescription"); // description
+            sb.AppendLine("5"); // days
+            sb.AppendLine("no"); // save
+            sb.AppendLine("no"); // make another task
+
+            using (var sw = new StringWriter())
+            {
+                using (var sr = new StringReader(sb.ToString()))
+                {
+                    Console.SetOut(sw);
+                    Console.SetIn(sr);
+
+                    TodoApp.ConsoleApp.ConsoleAppMain.Main(new string[] { "-path ../../../tasks.xml" });
+                    var result = sw.ToString();
+
+                    Assert.Contains("Task 1:\nLearn XML", result);
+                    Assert.Contains("Task 2:\nCoffe Time", result);
+                    Assert.Contains("Good luck!", result);
+                    Assert.Contains("Press any key to exit", result);
+                }
+            }
+        }
+
+        [Fact]
+        public void Test_add_task_save_and_verify()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("1"); // manipulate
+            sb.AppendLine("1"); // make
+            sb.AppendLine(testTaskName); // title
+            sb.AppendLine("MyDescription"); // description
+            sb.AppendLine("5"); // days
+            sb.AppendLine("yes"); // save
+            sb.AppendLine("no"); // make another task
+
+            using (var sw = new StringWriter())
+            {
+                using (var sr = new StringReader(sb.ToString()))
+                {
+                    Console.SetOut(sw);
+                    Console.SetIn(sr);
+
+                    TodoApp.ConsoleApp.ConsoleAppMain.Main(new string[] { "-path ../../../tasks.xml" });
+                    var result = sw.ToString();
+
+                    Assert.Contains("Task 1:\nLearn XML", result);
+                    Assert.Contains("Task 2:\nCoffe Time", result);
+                    Assert.Contains("Good luck!", result);
+                    Assert.Contains("Press any key to exit", result);
+                }
+            }
+
+            // verify task was saved to file
+            using (var sw = new StringWriter())
+            {
+                using (var sr = new StringReader(string.Empty))
+                {
+                    Console.SetOut(sw);
+                    Console.SetIn(sr);
+
+                    TodoApp.ConsoleApp.ConsoleAppMain.Main(new string[] { "-path ../../../tasks.xml" });
+                    var result = sw.ToString();
+
+                    Assert.Contains("Task 1:\nLearn XML", result);
+                    Assert.Contains("Task 2:\nCoffe Time", result);
+                    Assert.Contains($"Task 3:\n{testTaskName}", result);
+                }
+            }
+        }
+
+        [Fact]
+        public void Test_delete_and_verify()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("1"); // manipulate
+            sb.AppendLine("2"); // delete
+            sb.AppendLine("3"); // number of task
+
+            using (var sw = new StringWriter())
+            {
+                using (var sr = new StringReader(sb.ToString()))
+                {
+                    Console.SetOut(sw);
+                    Console.SetIn(sr);
+
+                    TodoApp.ConsoleApp.ConsoleAppMain.Main(new string[] { "-path ../../../tasks.xml" });
+                    var result = sw.ToString();
+
+                    Assert.Contains("Task 1:\nLearn XML", result);
+                    Assert.Contains("Task 2:\nCoffe Time", result);
+                }
+            }
+
+            // verify task was saved to file
+            using (var sw = new StringWriter())
+            {
+                using (var sr = new StringReader(string.Empty))
+                {
+                    Console.SetOut(sw);
+                    Console.SetIn(sr);
+
+                    TodoApp.ConsoleApp.ConsoleAppMain.Main(new string[] { "-path ../../../tasks.xml" });
+                    var result = sw.ToString();
+
+                    Assert.Contains("Task 1:\nLearn XML", result);
+                    Assert.Contains("Task 2:\nCoffe Time", result);
+                    Assert.DoesNotContain($"Task 3:\n{testTaskName}", result);
                 }
             }
         }
