622de4515178ed81b8f03b9639c46a1e82a932cb
Vencislav Evlogiev <redsartvimes@yahoo.com>
2020-02-03 20:49:54 +0200

    Add Readtask and GetAll methods to XMLRepository.

--- TodoApp.ConsoleApp/Repositories/IRepository.cs
+++ TodoApp.ConsoleApp/Repositories/IRepository.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace TodoApp.ConsoleApp.Repositories
 {
-    class Irepository
+    interface IRepository<T>
     {
+        T Get(int id);
+        IEnumerable<T> GetAll();
+
+        void Add(T entity);
+        void Update(T entity);
+        void Remove(T entity);
     }
 }
---
+++ TodoApp.ConsoleApp/Repositories/XMLRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TodoApp.ConsoleApp.Repositories
+{
+    abstract class XMLRepository<T> : IRepository<T>
+    {
+        const string path = "../../tasks.xml";
+        public virtual XElement ParentElement { get; protected set; }
+
+        protected XName ElementName { get; private set; }
+
+        protected abstract Func<XElement, T> Selector { get; }
+
+        protected abstract void SetXElementValue(T model, XElement element);
+
+        protected abstract XElement CreateXElement(T model);
+
+        protected abstract object GetEntityId(T entidade);
+        public void Add(T entity)
+        {
+            ParentElement.Add(CreateXElement(entity));
+        }
+
+        public T Get(int id)
+        {
+            return 
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return ParentElement.Elements(ElementName).Select(Selector);
+        }
+
+        public virtual IEnumerable<T> GetAll(object parentId)
+        {
+            throw new InvalidOperationException("This entity doesn't contains a parent.");
+        }
+        public void Remove(T entity)
+        {
+            ParentElement.Elements().FirstOrDefault(a => a.Attribute("Id").Value == GetEntityId(entity).ToString()).Remove();
+        }
+
+        public void Update(T entity)
+        {
+            SetXElementValue(entity,
+                            ParentElement.Elements().FirstOrDefault(a => a.Attribute("Id").Value == GetEntityId(entity).ToString()
+));
+        }
+    }
+}
