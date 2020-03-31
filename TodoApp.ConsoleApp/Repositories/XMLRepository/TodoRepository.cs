7243c56eda5d13da196c65a3dbf9ed6e82acad2a
Redsart <redsartvimes@yahoo.com>
2020-03-31 17:26:27 +0300

    Change the implementation of ElementToEntity and EntityToElement methods.

--- TodoApp.ConsoleApp/Repositories/XMLRepository/TodoRepository.cs
+++ TodoApp.ConsoleApp/Repositories/XMLRepository/TodoRepository.cs
@@ -2,14 +2,12 @@
 using System.Xml.Linq;
 using TodoApp.ConsoleApp.Repositories.Models;
 using TodoApp.ConsoleApp.Repositories.Interfaces;
-using System.IO;
-using System.Globalization;
 
 namespace TodoApp.ConsoleApp.Repositories.XMLRepository
 {
     public class TodoRepository : RepositoryBase<TodoModel, Guid>, ITodoRepository
     {
-        string idName = "id";
+        string idName = "Id";
         public TodoRepository(string path) : base(path, "todos")
         {
 
@@ -30,22 +28,14 @@ namespace TodoApp.ConsoleApp.Repositories.XMLRepository
                 return null;
             }
 
-            string title = element.Element("title").Value;
-            string description = element.Element("description").Value;
-            string start = element.Element("createdOn").Value;
-            DateTime dateCreated = DateTime.ParseExact(start, "dd MM yyyy", CultureInfo.InvariantCulture);
-            string end = element.Element("dueDate").Value;
-            DateTime dueDate= DateTime.ParseExact(end, "dd MM yyyy", CultureInfo.InvariantCulture);
-            Guid id = Guid.Parse(element.Attribute(IdName).Value);
+            var entity = new TodoModel();
+            entity.Title = XmlParser.GetString(element, "Title");
+            entity.Description = XmlParser.GetString(element, "Description");
+            entity.CreatedOn = XmlParser.GetDateTime(element, "CreatedOn");
+            entity.DueDate = XmlParser.GetDateTime(element, "DueDate");
+            entity.ID = XmlParser.GetGuid(element, IdName);
 
-            var model = new TodoModel();
-            model.Title = title;
-            model.Description = description;
-            model.CreatedOn = dateCreated;
-            model.DueDate = dueDate;
-            model.ID = id;
-
-            return model;
+            return entity;
         }
 
         protected override XElement EntityToElement(TodoModel entity)
@@ -56,17 +46,11 @@ namespace TodoApp.ConsoleApp.Repositories.XMLRepository
             }
 
             var element = new XElement("todo");
-            var title = new XElement("title", entity.Title);
-            var description = new XElement("description", entity.Description);
-            var dateCreated = new XElement("createdOn", string.Format("{0:dd MM yyyy}", entity.CreatedOn));
-            var dueDate = new XElement("dueDate", string.Format("{0:dd MM yyyy}", entity.DueDate));
-            var id = new XAttribute("id", entity.ID);
-
-            element.Add(title);
-            element.Add(id);
-            element.Add(description);
-            element.Add(dateCreated);
-            element.Add(dueDate);
+            XmlParser.SetString(element, "Title", entity.Title);
+            XmlParser.SetString(element, "Description", entity.Description);
+            XmlParser.SetDateTime(element, "CreatedOn", entity.CreatedOn);
+            XmlParser.SetDateTime(element, "DueDate", entity.DueDate);
+            XmlParser.SetGuid(element, IdName, entity.ID);
 
             return element;
         }
