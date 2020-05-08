28c7946189d7822244cc3d59d0448094e2c9a8e7
Redsart <redsartvimes@yahoo.com>
2020-05-08 23:36:28 +0300

    Add XmlContext and IXmlContext.

---
+++ TodoApp.Repositories/XmlRepository/Utils/XmlContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml.Linq;
+
+namespace TodoApp.Repositories.XmlRepository.Utils
+{
+    class XmlContext : IXmlContext
+    {
+        static string Path { get; set; }
+
+        static XDocument doc;
+
+        XmlContext(string path)
+        {
+            Path = path;
+            doc = XDocument.Load(path);
+        }
+
+        public XElement GetContainer(XName name)
+        {
+            XElement containerElement = doc.Element(name);
+
+            return containerElement;
+        }
+
+        public void Save()
+        {
+            doc.Save(Path);
+        }
+    }
+}
