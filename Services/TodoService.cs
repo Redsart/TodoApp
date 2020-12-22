4c1818ceaba815a169e2006e4bb886bc250fefc9
Redsart <redsartvimes@yahoo.com>
2020-12-22 18:51:46 +0200

    Add new test project for TodoApp.Services.

---
+++ Services/TodoService.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+using Moq;
+using TodoApp.Repositories.Interfaces;
+
+namespace TodoApp.Tests.Services
+{
+    public class TodoService
+    {
+
+        protected Mock<ITodoRepository> MockService;
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
