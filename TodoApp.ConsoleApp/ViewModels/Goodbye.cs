c1a898a0061438c9fd236f625b8821a3e78a23a3
Redsart <redsartvimes@yahoo.com>
2021-04-22 17:48:57 +0300

    Add Goodbye class and implementation.

---
+++ TodoApp.ConsoleApp/ViewModels/Goodbye.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.ConsoleApp.Framework;
+using Microsoft.Extensions.Hosting;
+
+namespace TodoApp.ConsoleApp.ViewModels
+{
+    class GoodBye : ViewModel
+    {
+        IHostApplicationLifetime AppLifetime;
+
+        public Goodbye(IHostApplicationLifetime appLifetime)
+        {
+            AppLifetime = appLifetime;
+        }
+
+        public void Exit()
+        {
+
+            AppLifetime.StopApplication();
+        }
+    }
+}
