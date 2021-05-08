35cc3fa20faa6b579c6b2b9c1de30e814b3eb327
Redsart <redsartvimes@yahoo.com>
2021-05-08 11:18:54 +0300

    Add Views.Goodbye class.

--- TodoApp.ConsoleApp/ViewModels/Goodbye.cs
+++ TodoApp.ConsoleApp/ViewModels/Goodbye.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TodoApp.ConsoleApp.Framework;
 using Microsoft.Extensions.Hosting;
 
 namespace TodoApp.ConsoleApp.ViewModels
 {
-    class GoodBye : ViewModel
+    class Goodbye : ViewModel
     {
         IHostApplicationLifetime AppLifetime;
 
--- TodoApp.ConsoleApp/ViewModels/Navigation.cs
+++ TodoApp.ConsoleApp/ViewModels/Navigation.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TodoApp.ConsoleApp.Framework;
 
 namespace TodoApp.ConsoleApp.ViewModels
 {
-    class Navigation
+    public class Navigation : ViewModel
     {
+        
     }
 }
