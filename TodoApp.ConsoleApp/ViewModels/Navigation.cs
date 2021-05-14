3769327af188bcff882bbb21d4f930dca58a903a
Redsart <redsartvimes@yahoo.com>
2021-05-14 16:20:11 +0300

    Add implementation for ViewModels.Navigation class.

--- TodoApp.ConsoleApp/ViewModels/Navigation.cs
+++ TodoApp.ConsoleApp/ViewModels/Navigation.cs
@@ -1,14 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TodoApp.ConsoleApp.Framework;
+using V = TodoApp.ConsoleApp.Views;
+using P = TodoApp.ConsoleApp.Props;
 
 namespace TodoApp.ConsoleApp.ViewModels
 {
     public class Navigation : ViewModel
     {
-        
+        protected readonly IRouter Router;
+
+        public Navigation(IRouter router)
+        {
+            Router = router;
+        }
+
+        public void OpenHome()
+        {
+            Router.Open<V.Home>();
+        }
+        public void OpenTodoDetails(int id)
+        {
+            Router.Open<V.TodoDetails, P.Todo>(new P.Todo(id));
+        }
+
+        public void Goodbye()
+        {
+            Router.Open<V.Goodbye>();
+        }
+
+        public bool CanGoBack()
+        {
+            return Router.CanGoTo(-1);
+        }
+
+        public void GoBack()
+        {
+            Router.GoTo(-1);
+        }
     }
 }
--- TodoApp.ConsoleApp/ViewModels/Todo.cs
+++ TodoApp.ConsoleApp/ViewModels/Todo.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TodoApp.ConsoleApp.Framework;
 using P = TodoApp.ConsoleApp.Props;
 using TodoApp.Services;
